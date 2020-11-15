1bae162e4fdcb8a4468f654c70e7fa928d9b4530
Maldo601 <maldo601@gmail.com>
2020-11-15 17:51:44 +0100

    masters

--- src/F_Condicionals/A_ElPorter.java
+++ src/F_Condicionals/A_ElPorter.java
@@ -3,11 +3,15 @@ package F_Condicionals;
 import java.util.Scanner;
 
 public class A_ElPorter {
-    public static Scanner lectexer;
+    public static Scanner port;
 
     public static void main(String[] args) {
-
-        lectexer = new Scanner(System.in);
+        port = new Scanner(System.in);
+        // Welcome Message.
+        System.out.print("\tHola sóc el porter de la discoteca." + "\n\tQuina edat tens?: ");
+        byte age = port.nextByte();
+        // Output amb Operador Ternari
+        System.out.println(age >= 18 ? "Ets major d'edat, passa." : "Ets menor d'edat, no pots passar.");
 
     }
 }
