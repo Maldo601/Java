94d087b6b5169c5486ff52e00f1bfb3c12c4dcd1
Maldo601 <maldo601@gmail.com>
2020-11-15 16:33:21 +0100

    masters

---
+++ src/F_Condicionals/A_ElPorter.java
@@ -0,0 +1,13 @@
+package F_Condicionals;
+
+import java.util.Scanner;
+
+public class A_ElPorter {
+    public static Scanner lectexer;
+
+    public static void main(String[] args) {
+
+        lectexer = new Scanner(System.in);
+
+    }
+}
