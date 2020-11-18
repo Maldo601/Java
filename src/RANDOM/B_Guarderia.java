6e3aa5eb0bb5d92c21357c2e4101c476506b057f
Maldo601 <maldo601@gmail.com>
2020-11-18 17:37:33 +0100

    Masters

---
+++ src/RANDOM/B_Guarderia.java
@@ -0,0 +1,32 @@
+package RANDOM;
+/*
+1) Un col·legi-guarderia fa un descompte als nens nouvinguts en funció de l'edat.
+De 0 a 3 anys - 10 % de descompte.
+4 i 5 anys - 15 % de descompte.
+6 anys - 20 % de descompte.
+7 anys - 15 % de descompte.
+Altres edats - Cap descompte.
+Fer un programa en Java que demani l'edat del nano i mostri el descompte que se li aplicarà i 
+també el cost mensual tenint en compte que sense descompte s'han de pagar 500 € al mes
+*/
+import java.util.Scanner; 
+
+public class B_Guarderia {
+    public static Scanner lect;
+    public static void main (String[] args ) {
+        lect = new Scanner(System.in);
+        final Double mat = (double) 500;
+        System.out.print("Introdueix edat: ");
+        int age = lect.nextInt();
+        if (age >= 0 || age <= 3)          
+            System.out.println(mat/20);
+            else if ( age >= 4 || age <= 5)
+            System.out.println(mat/15);
+            else if ( age == 6 )
+            System.out.println(mat/20);
+            else if (age == 7 )
+            System.out.println(mat/15);
+            else
+            System.out.println("No hi ha descompte");  
+    }
+}
--- src/RANDOM/random.java
+++ src/RANDOM/random.java
@@ -4,20 +4,29 @@ import java.util.Scanner;
 
 public class random {
     public static Scanner random;
+  
     public static void main (String[] args) {
         random = new Scanner(System.in);
 
-        int edad = random.nextInt();
-            System.out.print("Introdueix la teua edat: " + (edad+1) + "años");
-            String s = random.nextLine();
-            
-            System.out.println("introduce tu nombre: " + s );
-            //s = s.replace(',','.');
-            //double edad = Double.parseDouble(s);
-           
-
-
-
-        System.out.println();
+        long x,y,z;
+        System.out.print("Introdueix x: ");
+        x = random.nextLong();
+        System.out.print("Introdueix y: ");
+        y = random.nextLong();
+        System.out.print("Introdueix z: ");
+        z = random.nextLong();
+        // Condicionals
+        if ((x == y) && ( x == z) && (y == z) )
+            System.out.println("El triangle és Equilater");
+            else if ((x == y) && (x != z) && (y != z))
+                System.out.println("El triangle és Isoceles");
+            else if ((x != y) && (x != z) && (y != z))
+                System.out.println("El triangle és Escalè");
+        // Condicional Pitagores
+        if  ( x*x + y*y == z*z )
+            System.out.print("El triangle és rectangle.");
+            else
+            System.out.println("El triangle no és rectangle.");
     }
 }
+// _ M A L D O _ // 
