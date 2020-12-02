cafe0d287093622355f93b6b6ab764f76a06aadc
Maldo601 <maldo601@gmail.com>
2020-12-02 17:14:21 +0100

    Masters

---
+++ src/F_Condicionals/F_BMod.java
@@ -0,0 +1,17 @@
+package F_Condicionals;
+
+import java.util.Scanner;
+
+public class F_BMod {
+    public static Scanner lect;
+    public static void main (String [] args) {
+        lect = new Scanner(System.in);
+        // Variables
+
+
+
+
+
+        //Outputs
+    }
+}
---
+++ src/RANDOM/random1.java
@@ -0,0 +1,23 @@
+package RANDOM;
+// ARRAYS 
+import java.util.Scanner;
+public class random1 {
+    public static Scanner lect;
+    public static void main (String[] args) {
+        lect = new Scanner(System.in);
+        // Variables
+        String l = lect.nextLine();
+        // Condicionals
+        if ( l.equals("A") || l.equals("E") || l.equals("I") || l.equals("O") || l.equals("U") )
+            System.out.println("La lletra "+ l + " ... es majuscula i vocal.");
+            else if ( l.equals ("a") || l.equals("e") || l.equals("i") || l.equals("o") || l.equals("u"))
+            System.out.println("La lletra "+ l + " ... es minuscula i vocal");
+            else if ( l.equals("Z"))
+            System.out.println("La lletra "+ l + " ...es la ultima lletra i mayuscula");
+            else if (l.equals("z"))
+            System.out.println("La lletra "+ l + " ...es la ultima lletra i minuscula ");
+        else
+        System.out.println( "La lletra "+ l + " ...No entra dintre els meus parametres.");
+    }
+}
+// MALDO // 
