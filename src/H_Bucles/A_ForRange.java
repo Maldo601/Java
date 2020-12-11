58b6d4d34d83471e1ef03d5fc4235ee2093ccb70
Maldo601 <maldo601@gmail.com>
2020-12-11 21:00:49 +0100

    Masters

--- src/H_Bucles/A_ForRange.java
+++ src/H_Bucles/A_ForRange.java
@@ -1,5 +1,9 @@
 package H_Bucles;
 
 public class A_ForRange {
-    
+    public static void main (String[] args) {
+        // Bucle 
+        for (int n =100; n<=110;n++)
+            System.out.println(n+"");
+    }
 }
--- src/H_Bucles/B_ForRangeInverted.java
+++ src/H_Bucles/B_ForRangeInverted.java
@@ -1,5 +1,9 @@
 package H_Bucles;
 
 public class B_ForRangeInverted {
-    
+    public static void main (String[] args) {
+            // Bucle 
+            for (int n =512; n>=500;n--)
+                System.out.println(n+"");
+    }
 }
