9d64b22613c5c455cfdce1453e9c79f345bf2634
Maldo601 <maldo601@gmail.com>
2020-12-11 21:39:20 +0100

    Masters

---
+++ src/H_Bucles/F_DoWhile.java
@@ -0,0 +1,11 @@
+package H_Bucles;
+
+public class F_DoWhile {
+    public static void main (String[] args) {
+        int x = 100;
+        do {
+            System.out.println(x + " ");
+            x++;
+        } while (x<=110);
+    }
+}
