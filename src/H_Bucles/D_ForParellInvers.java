361ee5ceab46e0a1955e775094795f63038abfde
Maldo601 <maldo601@gmail.com>
2020-12-11 21:11:45 +0100

    Masters

--- src/H_Bucles/D_ForParellInvers.java
+++ src/H_Bucles/D_ForParellInvers.java
@@ -1,5 +1,9 @@
 package H_Bucles;
 
 public class D_ForParellInvers {
-    
+    public static void main (String[] args) {
+        // Bucle 
+        for (int n =200;n>=50;n-=2)
+            System.out.print(n+" ");
+    }
 }
