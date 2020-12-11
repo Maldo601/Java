168cdc2739279864bc53c24e3d08637afab5f922
Maldo601 <maldo601@gmail.com>
2020-12-11 21:25:15 +0100

    Masters

--- src/H_Bucles/E_WhileRange.java
+++ src/H_Bucles/E_WhileRange.java
@@ -1,5 +1,11 @@
 package H_Bucles;
 
 public class E_WhileRange {
-    
+    public static void main (String [] args){
+        int x = 99;
+        while (x<110) {
+            x++;
+            System.out.println(x);
+        }
+    }
 }
