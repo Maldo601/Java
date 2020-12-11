46669ccc187b138860673514060d381b3ff8a6ce
Maldo601 <maldo601@gmail.com>
2020-12-11 21:31:09 +0100

    Masters

--- src/H_Bucles/E_WhileRange.java
+++ src/H_Bucles/E_WhileRange.java
@@ -2,6 +2,9 @@ package H_Bucles;
 
 public class E_WhileRange {
     public static void main (String [] args){
+        /*Iniciem la variable a 99 i no li passarem un igual al 110
+          sino incrementara un nombre per davant al executarse despres del 
+          bucle.  */
         int x = 99;
         while (x<110) {
             x++;
