4d9191719c37698567e1ea573ddb5bce76268b6c
Maldo601 <maldo601@gmail.com>
2020-12-12 01:18:58 +0100

    Masters

--- src/H_Bucles/I_ComboNum.java
+++ src/H_Bucles/I_ComboNum.java
@@ -1,5 +1,17 @@
 package H_Bucles;
 
 public class I_ComboNum {
-    
+    public static void main (String[] args) {
+        
+        // Anidació de bucle
+        for (int n=1; n<=9; n++){
+            for (int m=0; m<10; m++){
+                System.out.println(n + "" + m);
+            }
+        }
+        // Bucle Normal per fer totes les combinacions de dos xifres:
+        for (int j=10; j<100; j++)       
+          System.out.print(j + " ");
+        
+    }
 }
