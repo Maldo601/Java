1864d2bec16092142434765a9c1da24bb000d9a4
Maldo601 <maldo601@gmail.com>
2020-12-12 03:33:26 +0100

    Masters

--- src/H_Bucles/K_SumaProductes.java
+++ src/H_Bucles/K_SumaProductes.java
@@ -1,5 +1,14 @@
 package H_Bucles;
 
 public class K_SumaProductes {
-    
+    public static void main (String[] args){
+        byte m =2;
+        short sum = 5;
+        while( (Math.sqrt(sum) != (byte)Math.sqrt(sum)) ){
+            m++;
+            sum += m*m;
+            System.out.println(sum);
+        }
+
+    }
 }
