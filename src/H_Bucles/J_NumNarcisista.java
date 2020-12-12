dc30779fe92caff362d515aad191061adc7cb508
Maldo601 <maldo601@gmail.com>
2020-12-12 02:41:14 +0100

    Masters

--- src/H_Bucles/J_NumNarcisista.java
+++ src/H_Bucles/J_NumNarcisista.java
@@ -1,5 +1,18 @@
 package H_Bucles;
 
 public class J_NumNarcisista {
-    
+    public static void main (String[] args){
+        // Bucle Narcisista.
+       int n;
+       for(n=100; n<=500; n++){
+           int c = n / 100;
+           int res = n % 100;
+           int d = res/10;
+           int u = res%10;
+           if (n == Math.pow(c,3)+Math.pow(d,3)+Math.pow(u,3))
+            System.out.println(n);
+       }
+       
+
+    }
 }
--- src/H_Bucles/L_ComboNumTres.java
+++ src/H_Bucles/L_ComboNumTres.java
@@ -1,5 +1,13 @@
 package H_Bucles;
 
 public class L_ComboNumTres {
-    
+    public static void main (String[] args) {
+        for (int n=1; n<=2; n++){
+            for (int m=0; m<=2; m++){
+                for(int i=0; i<=2; i++){
+                    System.out.println(n+""+m+""+i);
+                }
+            }
+        }
+    }
 }
