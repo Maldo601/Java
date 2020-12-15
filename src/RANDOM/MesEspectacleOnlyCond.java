68db82a55dcc6313047c95b97db2e1ef8f46f735
Maldo601 <maldo601@gmail.com>
2020-12-15 14:56:54 +0100

    Masters

---
+++ src/RANDOM/MesEspectacleOnlyCond.java
@@ -0,0 +1,17 @@
+package RANDOM;
+import java.util.Scanner;
+public class MesEspectacleOnlyCond {
+    public static Scanner lect;
+    public static void main (String[] args){
+        lect = new Scanner(System.in);
+        System.out.print("Hola quants adults sou?: ");
+        int adults = lect.nextInt();
+        System.out.println("I quants menors porteu?: ");
+        int menors = lect.nextInt();
+        
+        if ( menors > adults*2)
+            System.out.println("No podeu passar, hi han " + menors + " menors i " +adults+ " adults");
+        else 
+            System.out.println("Podeu passar, hi han "+menors+" menors i "+adults+ " adults");
+    }
+}
