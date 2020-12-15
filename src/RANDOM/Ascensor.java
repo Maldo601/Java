3ce261ca25dec3c33b29424c5b4af225e3be350e
Maldo601 <maldo601@gmail.com>
2020-12-15 22:54:00 +0100

    Masters

--- src/RANDOM/Ascensor.java
+++ src/RANDOM/Ascensor.java
@@ -1,29 +1,34 @@
 package RANDOM;
 import java.util.Scanner;
-
-import javax.lang.model.util.ElementScanner6;
-
-import java.util.Random;
 public class Ascensor {
     public static Scanner lect;
-    public static Random rnd;
     public static void main (String[] args){
         lect = new Scanner(System.in);
-        rnd = new Random();
-        System.out.println("\nBenvingut al ascensor");
-        //String plantes
-        final int lvl [] = {"S4","S3","S2","S1","PB","P1","P2","P3","P4","P5","P6","P7","P8","P9"}; // 28
-        int actual = lvl[new Random().nextInt(lvl.length)];
-        System.out.println("\nL'ascensor està a la planta: " + actual + "\n" );
-        System.out.println("A quina planta vols anar?: ");
-        int userGo = lect.nextInt();
-        if (user == 12 || user == 13 )
-            System.out.println("Area Restringida a usuaris.");
-        else if (userGo > actual)
-            System.out.println("Pujant desde: " + actual + " fins a: " +userGo);
-        else if (usergo < actual)
-            System.out.println("Baixant desde: "+ actual + " fins a " +userGo);
-        else 
-            System.out.println("Ens quedarem on estem crec."); 
-    }
+		// String Pisos.
+		final String lvls = "S4S3S2S1PBP1P2P3P4P5P6P7P8P9";
+		int pisAle = (int)(Math.random()*14);
+		String nomPis = lvls.substring(pisAle*2,pisAle*2+2);
+		System.out.println("Estas a " + nomPis);
+		System.out.println("Selecciona on vols anar: ");
+		String dest = lect.nextLine();
+		if ( dest.equals(nomPis) )
+			System.out.println("ja estem en aquesta planta");
+			else if ( dest.equals("P8") || dest.equals("P9"))
+				System.out.println("Area restringida");
+			else if ( lvls.indexOf(dest) < 0 )
+				System.out.println("Error.");
+			else if ( lvls.indexOf(dest) > lvls.indexOf(nomPis))
+				System.out.println("Muntant a... " + dest);
+			else 
+				System.out.println("Baixant a... " + dest);
+	}
+	
 }
+    
+ /*  FAIL CODE
+		
+     //String plantes
+        // {"S4","S3","S2","S1","PB","P1","P2","P3","P4","P5","P6","P7","P8","P9"};
+        String[] lvl = {"S4","S3","S2","S1","PB","P1","P2","P3","P4","P5","P6","P7","P8","P9"}; // 28
+*/
+       
