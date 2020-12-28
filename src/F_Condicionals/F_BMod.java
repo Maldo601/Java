159e03eff9ed2973e6c1f6ef0c132eaf32729362
Maldo601 <maldo601@gmail.com>
2020-12-28 21:15:18 +0100

    Masters

--- src/F_Condicionals/F_BMod.java
+++ src/F_Condicionals/F_BMod.java
@@ -1,25 +1,23 @@
 package F_Condicionals;
-
 import java.util.Scanner;
-
 public class F_BMod {
     public static Scanner lect;
     public static void main (String[] args) {
         lect = new Scanner(System.in);
         // Variable d'almacenament d'input.
-        System.out.print("Introdueix una nota entre 1 i 10 sense decimals: ");
+        System.out.print("\n\t\033[36mIntrodueix una nota entre 1 i 10 sense decimals: \u001B[0m");
         double age = lect.nextDouble();
         String ageCad = String.valueOf(age);  
         // Condicionals 
-        if (ageCad.length() <= 5) // [Desde 1.00 a 9.99] 10 enter son 2 caracters i 10.01 (5 length) excedix el que demana el programa i el trencara. 
+        if (ageCad.length() <= 4) // [Desde 1.00 a 9.99] 10 enter son 2 caracters i 10.01 (5 length) excedix el que demana el programa i el trencara. 
             if ((age == (int)age) && age >= 1 && age <= 10)
-            System.out.println("Has introduit un enter vàlid al rang. És: " + (int)age);
+            System.out.println("\n\t\033[32mHas introduit un enter vàlid al rang. És: \u001B[0m" + (int)age + "\n");
             else if (age < 1 || age > 10)
-            System.out.println("Estas fora del rang solicitat.");
+            System.out.println("\n\t\033[31mEstas fora del rang solicitat.\n");
             else 
-            System.out.println("Has introduit un numero decimal. És el: " + age);
+            System.out.println("\n\t\033[31mHas introduit un numero decimal. És el: \u001B[0m" + age + "\n");
         else
-        System.out.println("ERROR, Excedeix la longitud de valor valid");
+        System.out.println("\n\t\033[31mERROR, Excedeix la longitud de valor valid\n");
     }
 }
 /*
