b7189d52b3cec3d16fb7303c5f1e74c8cb3b6630
Maldo601 <maldo601@gmail.com>
2021-01-03 22:21:18 +0100

    Masters

--- src/I_Arrays/B_Repetits.java
+++ src/I_Arrays/B_Repetits.java
@@ -1,8 +1,13 @@
 package I_Arrays;
+
 import java.util.Scanner;
 public class B_Repetits {
     public static Scanner lect;
+
     public static void main (String[] args){
         lect = new Scanner(System.in);
+        int matriz[][] = new int[(int) (Math.random()*9+1)][(int) (Math.random()*9+1)];
+        
+        System.out.print(matriz);
     }
 }
