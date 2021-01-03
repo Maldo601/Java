622c031495d5458d75451d9a6353687de7db652f
Maldo601 <maldo601@gmail.com>
2021-01-03 20:14:21 +0100

    Masters

---
+++ src/I_Arrays/A_ReinaAlfil.java
@@ -0,0 +1,8 @@
+package I_Arrays;
+import java.util.Scanner;
+public class A_ReinaAlfil {
+    public static Scanner lect;
+    public static void main (String[] args){
+        lect = new Scanner(System.in);
+    }
+}
---
+++ src/I_Arrays/B_Repetits.java
@@ -0,0 +1,8 @@
+package I_Arrays;
+import java.util.Scanner;
+public class B_Repetits {
+    public static Scanner lect;
+    public static void main (String[] args){
+        lect = new Scanner(System.in);
+    }
+}
