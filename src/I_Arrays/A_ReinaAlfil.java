381cc1e02b35cceb0a6d8dfa23bb6e36efacb436
Maldo601 <maldo601@gmail.com>
2021-01-05 14:08:30 +0100

    Masters

--- src/I_Arrays/A_ReinaAlfil.java
+++ src/I_Arrays/A_ReinaAlfil.java
@@ -4,5 +4,21 @@ public class A_ReinaAlfil {
     public static Scanner lect;
     public static void main (String[] args){
         lect = new Scanner(System.in);
+        boolean v[][] = new boolean[3][3];
+        System.out.print("\t\033[33mPosicio de la reina: \u001B[0m");
+        int rX = lect.nextInt();
+        int rY = lect.nextInt();
+        System.out.print("\t\033[33mPosicio de alfil: \u001B[0m");
+        int aX = lect.nextInt();
+        int aY = lect.nextInt();
+        
+        for (int i = 0; i < v.length; i++) {
+            for (int j = 0; j < v[0].length; j++) {
+                
+                System.out.print("\t" + v[i][j] +" ");
+            }
+            System.out.println("\n");
+        }
+        
     }
 }
