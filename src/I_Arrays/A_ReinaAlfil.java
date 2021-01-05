786b2492581f8de45820c079fde9c9cbfeb60863
Maldo601 <maldo601@gmail.com>
2021-01-05 14:29:29 +0100

    Masters

--- src/I_Arrays/A_ReinaAlfil.java
+++ src/I_Arrays/A_ReinaAlfil.java
@@ -5,13 +5,19 @@ public class A_ReinaAlfil {
     public static void main (String[] args){
         lect = new Scanner(System.in);
         boolean v[][] = new boolean[3][3];
-        System.out.print("\t\033[33mPosicio de la reina: \u001B[0m");
+        // Bloc de peticions de posició.
+        System.out.print("\t\033[33mPosicio \"x\" de la Reina: \u001B[0m");
         int rX = lect.nextInt();
+        System.out.print("\t\033[33mPosicio \"y\" de la Reina: \u001B[0m");
         int rY = lect.nextInt();
-        System.out.print("\t\033[33mPosicio de alfil: \u001B[0m");
+        System.out.print("\t\033[33mPosicio \"x\" de la Reina: \u001B[0m");
         int aX = lect.nextInt();
+        System.out.print("\t\033[33mPosicio \"y\" de la Reina: \u001B[0m");
         int aY = lect.nextInt();
-        
+        // Colocador de peça.
+        v[rX][rY] = true; // Reina
+        v[aX][aY] = true; // Alfil
+        // Bucle Print.
         for (int i = 0; i < v.length; i++) {
             for (int j = 0; j < v[0].length; j++) {
                 
@@ -19,6 +25,6 @@ public class A_ReinaAlfil {
             }
             System.out.println("\n");
         }
-        
     }
 }
+// MALDO // 
