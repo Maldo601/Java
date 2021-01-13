2f3cc6fb5f795f3da8abb18d7a97e0b21b946b46
Maldo601 <maldo601@gmail.com>
2021-01-13 15:29:42 +0100

    Masters

--- src/I_Arrays/E_InterVect.java
+++ src/I_Arrays/E_InterVect.java
@@ -1,9 +1,8 @@
 package I_Arrays;
-import java.util.Arrays;
 public class E_InterVect {
     public static void main (String[] args){
         // Arrays. 
-        int v1[] = {1,2,3,4,5};
-        int v2[] = {1,2,3,4,5,6};
+        //int v1[] = {1,2,3,4,5};
+        //int v2[] = {1,2,3,4,5,6};
     }
 }
