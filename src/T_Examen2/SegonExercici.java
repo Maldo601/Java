3ad05c91c831ee99d15fdb7b80e347c5a90c49fc
Maldo601 <maldo601@gmail.com>
2021-01-27 10:29:57 +0100

    Masters

---
+++ src/T_Examen2/SegonExercici.java
@@ -0,0 +1,10 @@
+package T_Examen2;
+import java.util.Scanner;
+import java.util.Arrays;
+public class SegonExercici {
+    public static Scanner lect;
+    public static void main (String[] args){
+        lect = new Scanner(System.in);
+    }
+}
+
---
+++ src/T_Examen2/TercerExercici.java
@@ -0,0 +1,10 @@
+package T_Examen2;
+import java.util.Scanner;
+import java.util.Arrays;
+public class TercerExercici {
+
+    public static Scanner lect;
+    public static void main (String[] args){
+        lect = new Scanner(System.in);
+    }
+}
