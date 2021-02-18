c8764a59b7fa1c96efc4e2ab547dee56f794f37b
Maldo601 <maldo601@gmail.com>
2021-02-18 19:09:08 +0100

    Masters

--- src/L_Opcionals/B_OchoReinas.java
+++ src/L_Opcionals/B_OchoReinas.java
@@ -2,13 +2,11 @@ package L_Opcionals;
 public class B_OchoReinas {
     public static void main(String[] args){
         int t[] = {1,2,3,4,5,6,7,8};
-        
         /* 
         while(!correcte(t))
             t = shuffle(t);
         dibuixaTauler(t);
         */
-    
     }
     private static void dibuixaTauler(int[] t) {
     }
