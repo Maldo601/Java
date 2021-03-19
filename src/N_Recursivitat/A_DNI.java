9097bd289f5c43a82a35806313921bf12ee210ff
Maldo601 <maldo601@gmail.com>
2021-03-19 18:36:29 +0100

    Masters

--- src/N_Recursivitat/A_DNI.java
+++ src/N_Recursivitat/A_DNI.java
@@ -2,46 +2,103 @@ package N_Recursivitat;
 import java.util. *;
 public class A_DNI {
     static final int MAXINTERROGANTS = 6;
-    static String [] nifsOk = new String[(int) (Math.pow(10,MAXINTERROGANTS))];
+    static String [] nifsOk = new String[9];
     static int numNIFs = 0;
+    static String Validar = "";
     static Scanner lect;
+    static String v[] = new String [9];
     static String llegeixNif(){
         lect = new Scanner(System.in);
         System.out.print("Introdueix els caràcters del NIF incomplet, indicant en ? el caracter desconegut: ");
         String lectura = lect.nextLine();
                lectura.toUpperCase();
-            
-        // Retorna cadena en nif no resolt llegit i passada a MAYUS cuan es true
-        // Demana repetidament la introduccio de les dades si son false. 
-        // Per comprobar la cadena introduida usa la funcio nifCorrecte.
         return lectura;
     }
+    // Esta funció s'encarregarà de convertir els interrogants del bucle en números. La lletra no. 
+    static String aleatorietat (String nif){
+        String numP = "";
+        for ( int a = 0; a < nif.length(); a++){
+            numP = String.valueOf( (int) (Math.random() * (9-0+1)) + 0);
+        }
+        return numP;
+    }
     static boolean nifCorrecte(String nif){
-       
-
+        int interrogants = 0;
+        boolean interrogant = false;
+        boolean maxEnter = false;
+        boolean lastLetter = false;
+        // Al passar lletres retorna true, arreglar. 
+        // Funciona en true passantli
+        /*
+            - 8 digits 1 lletra
+            - 8 digits en interrogants i lletra
+            - 8 digits en interrogants i lletra interrogant
+        */
+        for ( int i = 0; i < nif.length(); i++){
+            if ( nif.charAt(i) == '?' )
+                interrogants++;
+                interrogant = true;
+            if (nif.charAt(i) <= 9 || nif.charAt(i) >= 0 || nif.charAt(i) == '?')
+                maxEnter = true;
+            if (!nif.substring(8).matches("\\d*"))
+                lastLetter = true;
+        }
+        if ( (interrogant && maxEnter && lastLetter) == true )
+            return true;
+        System.out.println(interrogants);
         return false;
+    }
+    /* Funció on es comprovarà el que hi ha al vector i es pendran mesures oportunes. Aquestes son: 
+        - 1_ Si hi ha lletra i hi ha una part numerica completa, es passarà a comprobar si es vàlid.
+        - 2_ Si falten digits a la part numèrica pero hi ha lletra, crida a Aleatorietat per generar els valors.
+             El vector ja amb valors i lletra, es passarà a la funció de validar la redundancia.
+
+             - 2.1_ Si es valid OK. 
+             - 2.2_ Si no es vàlid, tornarà a cridar a aleatorietat. 
+
+        - 3_ Si falta digit i lletra, reomplirem els buits dels digits i validarem cada una de les parts 
+             numeriques per extraure la lletra que li correspon.
+    */
+    static String[] addStringArray (String nif){   
+        if (nifCorrecte(nif) == true){
+            // Passa indiscriminadament una cadena al vector. 
+            for (int x = 0; x < nif.length(); x++){
+                nifsOk[x] = String.valueOf(nif.charAt(x));
+                v[x] = nifsOk[x];
+                Validar += nifsOk[x];
+            }
+            // Generadora de numeros aleatoris si hi han interrogants.
+            String numP = "";
+            for ( int y = 0; y < nif.length()-1; y++){
+                if (nifsOk[y].equals("?")){
+                    numP = String.valueOf( (int) (Math.random() * (9-0+1)) + 0);
+                    nifsOk[y] = numP;
+                }
+            }
+            // Generadora de lletra final. 
+        }
+        // LOGS 
+        System.out.println("\n\tDNI a Chain: " + Validar);
+        System.out.println("\n\t" + Arrays.toString(v));
+        return nifsOk;
+    }
+    static boolean validNif(String nif){
         
-    }
-    static void afegeixNif(String nif){
-        // 4 linies de codi
-    }
-    static boolean nifOk (String nif){
-
-        return true;
-    }
-    static void resolveNif(String nif){
-
+        // char letter = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(String.valueOf(validLetr % 23));
+        return false;
     }
     public static void main (String [] args){
         String nif = llegeixNif();
-        resolveNif(nif);
+        System.out.println("\n\t" + Arrays.toString(addStringArray(nif)));
+        /*resolveNif(nif);
         for (int n = 0; n < numNIFs; n++){
-            System.out.println("%6d - Trobat NIF: " + nifsOk[n] + "\n", n+1);
+            System.out.printf("Trobat NIF: " + nifsOk[n] + "\n", n+1);
         if (numNIFs == 0){
             System.out.println("No s'ha trobat cap solució que satisfagui els requeriments del problema");
         }
         else 
         System.out.println("Trobats un total de " + numNIFs + " NIF que satisfan els requeriments del problema");
         }
+        */
     }
 }
--- src/N_Recursivitat/provesDni.java
+++ src/N_Recursivitat/provesDni.java
@@ -5,78 +5,32 @@ public class provesDni {
     static int numNIFs = 0;
     static Scanner lect;
 // Filtros Condicionals.
-    static boolean nifCorrecte(String nif){
-        boolean llarg = false; boolean xifres = false; boolean maxInt = false; boolean trueLetter = false; 
-        // 9 CARACTERS DE LLARGARIA 
-        if (nif.length() == 9)
-            llarg = true;
-        else
-            return false;
-        // MAXIM INTERROGANTS
-        int counter = 0;
-        for (int i = 0; i < nif.length(); i++){
-            if ( nif.charAt(i) == '?' )
-                counter++;
-            if (nif.charAt(i) <= 9 || nif.charAt(i) >= 0 || nif.charAt(i) == '?')
-                maxInt = true;
-            else 
-                return false;
-        }
-        if ( counter <= 6 || counter == 0 )
-            xifres = true;
-        else
-            return false;
-        // ULTIMA LLETRA VALIDA O INTERROGANT ( No validada encara )
-        String lletresValides = "TRWAGMYFPDXBNJZSQVHLCKE";
-        for ( int k = 0; k < lletresValides.length(); k++ ){
-            if (nif.charAt(8) == '?' || nif.charAt(8) == lletresValides.charAt(k)){
-                trueLetter = true;
-            }
-        }
-        // LOGS 
-        System.out.println("\n\tInterrogants trobats: " + counter);
-        System.out.print("\n\tVector: " + Arrays.toString(afegeixNif(nif)));
-            if ( llarg && xifres && maxInt && trueLetter == true){
-                return true;
-            }
-        return false;
-    }
-// Passa el DNI a un vector. 
+    
     static String[] afegeixNif(String nif){
-        for (int x = 0; x < nif.length(); x++){
-            nifsOk[x] = String.valueOf(nif.charAt(x));
-            if (nifsOk[x].equals("?") && x < 8)
-                nifsOk[x] = aleatorietat(nif);
-        }
+        if (nifCorrecte(nif) == true){
+            for (int x = 0; x < nif.length(); x++){
+                nifsOk[x] = String.valueOf(nifOk(nif).charAt(x));
+                numNIFs ++;
+            }
+            for ( int y = 0; y < nif.length(); y ++){
+                if (nifsOk[y].equals("?") && y < 8)
+                    nifsOk[y] = aleatorietat(nif);
+            }
+        }     
         return nifsOk;
     }
 // Generador d'Enters Aleatoris (Apart, si detecta interrogants, es on els posa, excepte lletra.)
-    static String aleatorietat (String nif){
-        String numP = "";
-        for ( int a = 0; a < nif.length(); a++){
-            numP = String.valueOf( (int) (Math.random() * (9-0+1)) + 0);
-        }
-        return numP;
-    }
-    static boolean nifOk(String nif){
+    
+    static String nifOk(String nif){
         
-            String validLet="";
-            for (int l = 0; l < nif.length(); l++)
-                validLet += afegeixNif(nif)[l];
-
-            int numPart = 0;
-                numPart = Integer.parseInt(validLet.substring(0, 8));
-            if ( validLet.charAt(8) == '?'){
-                char letter = "TRWAGMYFPDXBNJZSQVHLCKE".charAt(numPart % 23);
-                validLet = String.valueOf(numPart) + Character.toString(letter);
-                for ( int i = 0; i < validLet.length(); i++){
-                    nifsOk[i] = String.valueOf(validLet.charAt(i));  
-                }
-                return true; 
+            i 
                 // Hasta aqui el programa torna un DNI Vàlid en base a la lletra
             }
-            System.out.println(validLet);
-        return false;
+        return nif;
+    }
+    static void resolveNif(String nif){
+        if (nifCorrecte(nif) == true)
+            afegeixNif(nif);
     }
     public static void main(String[] args){
         String lectura;
@@ -85,8 +39,8 @@ public class provesDni {
         lectura = lect.nextLine();
         
         // System.out.println(Arrays.toString(afegeixNif(lectura)));
-        System.out.println("\n\tValidesa: " + nifCorrecte(lectura));
-        System.out.println(nifOk(lectura));
+        resolveNif(lectura);
+        
     }
 }
 // Joan Marc Maldonado Negre 
