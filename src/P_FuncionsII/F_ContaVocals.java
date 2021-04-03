3546227221f5b1af3162921150a3c478d56330bf
Maldo601 <maldo601@gmail.com>
2021-04-03 23:06:30 +0200

    Masters

---
+++ src/P_FuncionsII/F_ContaVocals.java
@@ -0,0 +1,12 @@
+package P_FuncionsII;
+
+public class F_ContaVocals {
+
+  static int quantesVocals(String cad, String sonVocals) {
+    return 0;
+  }
+
+  public static void main(String[] args) {
+    System.out.println(quantesVocals("Ara no el trobarà", "aeiou"));
+  }
+}
