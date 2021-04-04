d9958008367b277d8a32ce02fb20f545adeb638d
Maldo601 <maldo601@gmail.com>
2021-04-04 20:16:30 +0200

    Masters

--- src/P_FuncionsII/J_ExplanadaBruixes.java
+++ src/P_FuncionsII/J_ExplanadaBruixes.java
@@ -1,5 +1,12 @@
 package P_FuncionsII;
 
 public class J_ExplanadaBruixes {
-    
+    static String explanadaBruixes ( int n ){
+
+        return "";
+    }
+    public static void main (String[ ] args){
+        System.out.println(explanadaBruixes(20));
+
+    }
 }
