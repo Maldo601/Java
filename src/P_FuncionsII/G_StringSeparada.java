8f9ea30b23d7e510864930d3940dde742d464171
Maldo601 <maldo601@gmail.com>
2021-04-04 18:12:50 +0200

    Masters

--- src/P_FuncionsII/G_StringSeparada.java
+++ src/P_FuncionsII/G_StringSeparada.java
@@ -2,7 +2,14 @@ package P_FuncionsII;
 
 public class G_StringSeparada {
     static String separaStr (String str, String sep){
-
-        return "";
+        String cad = "";
+        for (int i = 0; i < str.length(); i++)
+            cad += str.charAt(i) + sep;
+            
+        return cad.substring(0, cad.length()-2);
+    }
+    public static void main (String[] args){
+        System.out.println(separaStr("Hola", "--"));
     }
 }
+// Joan Marc Maldonado 
