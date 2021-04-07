0816cfeadbc9541b03a8fbfe9d419708600ba8c3
Maldo601 <maldo601@gmail.com>
2021-04-08 01:07:11 +0200

    Masters

---
+++ src/U_CodingBat/test.java
@@ -0,0 +1,11 @@
+package U_CodingBat;
+
+public class test {
+    // Rotar cadenes. 
+    public String left2(String str) {
+        return str.substring(2,str.length()) + str.substring(0,2);
+      }
+      
+    public static void main (String[] args){   
+    }
+}
