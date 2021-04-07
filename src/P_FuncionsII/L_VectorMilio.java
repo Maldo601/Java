697614a0a50169af5bb53e43142974344b159395
Maldo601 <maldo601@gmail.com>
2021-04-07 17:48:26 +0200

    Masters

---
+++ src/P_FuncionsII/L_VectorMilio.java
@@ -0,0 +1,16 @@
+package P_FuncionsII;
+
+public class L_VectorMilio {
+    static int max (double v[]){
+
+    }
+
+    public static void main (String [ ] args){
+        double [] v = new double [1000000];
+        for (int i = 0; i < v.length; i++){
+            v[i] = Math.random();
+        }
+        // long time = System.currentTimeMillis();
+
+    }
+}
