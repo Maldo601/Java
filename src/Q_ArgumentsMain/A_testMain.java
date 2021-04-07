708675a18b6d1a91db2513fbe27551682ec0dbbd
Maldo601 <maldo601@gmail.com>
2021-04-07 16:30:19 +0200

    Masters

--- src/Q_ArgumentsMain/A_testMain.java
+++ src/Q_ArgumentsMain/A_testMain.java
@@ -1,11 +1,18 @@
 package Q_ArgumentsMain;
-
+import java.util.*;
 public class A_testMain {
     /* 
 
     */
-    public static void main ( String [ ] args ) {
-
-        
+    public static void main ( String [] args ) {
+        if (args.length == 0)
+            System.out.println("\n\tT'has olvidat de passar parametros.");
+        else{
+            Arrays.sort(args);
+            // Bucle for - loop
+            for (String arg : args)
+               System.out.println(arg);
+        }
     }
 }
+// Maldo
---
+++ src/Q_ArgumentsMain/B_Ordena.java
@@ -0,0 +1,8 @@
+package Q_ArgumentsMain;
+import java.util.*;
+public class B_Ordena {
+    public static void main( String[] args){
+        for (String arg; args)
+            System.out.println();
+    }
+}
