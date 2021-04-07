7f158e445f86fca6998317af3f079bfe1b7abde1
Maldo601 <maldo601@gmail.com>
2021-04-07 16:41:00 +0200

    Masters

--- src/Q_ArgumentsMain/B_Ordena.java
+++ src/Q_ArgumentsMain/B_Ordena.java
@@ -2,7 +2,7 @@ package Q_ArgumentsMain;
 import java.util.*;
 public class B_Ordena {
     public static void main( String[] args){
-        for (String arg; args)
-            System.out.println();
+        for (String arg : args)
+            System.out.println(arg);
     }
 }
