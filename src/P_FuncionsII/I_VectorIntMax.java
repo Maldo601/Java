e854127821fda51d96770eae5dcd343defe51c8b
Maldo601 <maldo601@gmail.com>
2021-04-07 17:15:36 +0200

    Masters

--- src/P_FuncionsII/I_VectorIntMax.java
+++ src/P_FuncionsII/I_VectorIntMax.java
@@ -1,11 +1,14 @@
 package P_FuncionsII;
-
+import java.util.*;
 public class I_VectorIntMax {
-    static int max (int v[ ]){
-        return 0;
+    static int max (int v[]){
+        int aux[] = Arrays.copyOf(v, v.length);
+        Arrays.sort(aux);
+        return aux[aux.length-1];
     }
     public static void main (String [ ] args ){
-        int v[ ] = {1,2,3,4,5,6};
+        int v[ ] = {5,2,32,4,5,6};
         System.out.println(max(v));
     }
 }
+// Maldo
