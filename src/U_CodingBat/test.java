e2e8c8154f173fcfe50321c6976370f287905e8f
Maldo601 <maldo601@gmail.com>
2021-04-08 02:57:03 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -4,38 +4,45 @@ public class test {
     // Rotar cadenes. 
     public String left2(String str) {
         return str.substring(2,str.length()) + str.substring(0,2);
-      }
+    }
     // Rotar cadenes a la dreta "Hello -> loHel"
     public String right2(String str) {
         return str.substring(str.length()-2) + str.substring(0, str.length()-2);
-      }
+    }
     // True retorna primer caracter. False últim
     public String theEnd(String str, boolean front) {
         if (front == true)
           return ""+str.charAt(0);
         return str.substring(str.length()-1);
-      }
+    }
     // Retallar primer i ultima posicio. 
     public String withouEnd2(String str) {
         return (str.length() <= 2) ? ""
                                    : str.substring(1, str.length()-1) ;
-      }
+    }
     // Torna els dos caracters de la mitat d'una cadena
     public String middleTwo(String str) {
         String cad = str.substring( (str.length()/2)-1);
         return cad.substring(0,2);
-      }
+    }
     // Torna boolean si el patro acaba en "ly"
     public boolean endsLy(String str) {
         if ( str.length() < 2 )
           return false;
         return (str.substring(str.length()-2).equals("ly")) ? true 
                                                             : false;
-      }
+    }
     // Torna la cadena tallada per front i back (n)
     public String nTwice(String str, int n) {
         return str.substring(0,n) + str.substring(str.length()-n);
-      }
+    }
+    // Agafa partint de l'index un caracter mes. Si no es possible o l'index passat
+    // no es valid, torna els dos primers caracters de la cadena. 
+    public String twoChar(String str, int index) {
+    return ( str.length() > index+1 && index >= 0 ) ? str.substring(index, index+2)
+                                                    : str.substring(0, 2);
+    }
+      
       
       
       
