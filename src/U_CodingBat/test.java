71dbd5438985405ccc7561fba39f32583f7e3296
Maldo601 <maldo601@gmail.com>
2021-04-09 13:11:23 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -2,59 +2,59 @@ package U_CodingBat;
 
 public class test {
     // Rotar cadenes. 
-    public String left2(String str) {
+    static String left2(String str) {
         return str.substring(2,str.length()) + str.substring(0,2);
     }
     // Rotar cadenes a la dreta "Hello -> loHel"
-    public String right2(String str) {
+    static String right2(String str) {
         return str.substring(str.length()-2) + str.substring(0, str.length()-2);
     }
     // True retorna primer caracter. False últim
-    public String theEnd(String str, boolean front) {
+    static String theEnd(String str, boolean front) {
         if (front == true)
           return ""+str.charAt(0);
         return str.substring(str.length()-1);
     }
     // Retallar primer i ultima posicio. 
-    public String withouEnd2(String str) {
+    static String withouEnd2(String str) {
         return (str.length() <= 2) ? ""
                                    : str.substring(1, str.length()-1) ;
     }
     // Torna els dos caracters de la mitat d'una cadena
-    public String middleTwo(String str) {
+    static String middleTwo(String str) {
         String cad = str.substring( (str.length()/2)-1);
         return cad.substring(0,2);
         // Torna tres caracters del mig de una cadena:
         //     return cad.substring(0,3);
     }
     // Torna boolean si el patro acaba en "ly"
-    public boolean endsLy(String str) {
+    static boolean endsLy(String str) {
         if ( str.length() < 2 )
           return false;
         return (str.substring(str.length()-2).equals("ly")) ? true 
                                                             : false;
     }
     // Torna la cadena tallada per front i back (n)
-    public String nTwice(String str, int n) {
+    static String nTwice(String str, int n) {
         return str.substring(0,n) + str.substring(str.length()-n);
     }
     // Agafa partint de l'index un caracter mes. Si no es possible o l'index passat
     // no es valid, torna els dos primers caracters de la cadena. 
-    public String twoChar(String str, int index) {
+    static String twoChar(String str, int index) {
     return ( str.length() > index+1 && index >= 0 ) ? str.substring(index, index+2)
                                                     : str.substring(0, 2);
     }
     /* Retorna true o false si la string conté el patró "bad" al index 0 o 1.
        Té en compte casos d'error de pas de parametre. 
     */
-    public boolean hasBad(String str) {
+    static boolean hasBad(String str) {
         int i = str.indexOf("bad");
         if (i != -1 && i <= 1)
           return true;
         return false;
     }
     // Un altre exemple de l'anterior proposat per CodingBat diferent al meu. 
-    public boolean hasBad2(String str) {
+    static boolean hasBad2(String str) {
         if (str.length()>=3 && str.substring(0, 3).equals("bad")) 
           return true;    
         if (str.length()>=4 && str.substring(1, 4).equals("bad")) 
@@ -70,7 +70,7 @@ public class test {
     /* Retorna els dos primers caracters. Si nomes hi ha un caracter retorna caracter + @
        si no hi ha res, dos arrobes.
     */
-    public String atFirst(String str) {
+    static String atFirst(String str) {
         if (str.equals(""))
           return "@@";
         return (str.length() <= 1 ) ? str.substring(0) + "@" : str.substring(0,2) ;                         
@@ -79,7 +79,7 @@ public class test {
        Retorna la primera lletra de A i la ultima de B
        Si la cadena es buida es retorna una "@"
     */
-    public String lastChars(String a, String b) {
+    static String lastChars(String a, String b) {
         String cad = "";
         if ( a.equals("") && b.equals("") )
           return "@@";
