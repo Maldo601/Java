9336a8a9b23fe0e11f51af165f628b0037fe1278
Maldo601 <maldo601@gmail.com>
2021-04-11 17:42:14 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -431,9 +431,14 @@ public class test {
     : false ;
 }
   /*
-    Retorna 
+    Retorna True o False si el resto es igual al resto dels numeros de la dreta. 
   */
-  
+  static boolean lastDigit(int a, int b, int c) {
+    return (a % 10 == b % 10 || a % 10 == c % 10 || b % 10 == c % 10) 
+            ? true 
+            : false ;
+  }
+
   // MAIN 
   public static void main(String[] args) {
   }
