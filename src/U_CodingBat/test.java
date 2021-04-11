cd5c81da5397a86be9f6c81fd9f3d9c798dde788
Maldo601 <maldo601@gmail.com>
2021-04-11 15:18:25 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -370,6 +370,16 @@ public class test {
       return false;
     return (isMorning && !isMom) ? false : true;
   }
+  /*
+    Tea Party. 
+  */
+  static int teaParty(int tea, int candy) {
+    if ((tea < 5 || candy < 5))
+      return 0;
+    if (tea >= candy*2 || candy >= tea*2)
+      return 2;
+  return 1;
+}
 
   // MAIN 
   public static void main(String[] args) {
