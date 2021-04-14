d90bf58c9ee0c296c6434d61719a27fb018a4cb1
Maldo601 <maldo601@gmail.com>
2021-04-14 10:59:26 +0200

    Masters

--- src/P_FuncionsII/L_VectorMilio.java
+++ src/P_FuncionsII/L_VectorMilio.java
@@ -1,13 +1,27 @@
 package P_FuncionsII;
-
+import java.util.*;
 public class L_VectorMilio {
-    static int max (double v[]){
-        return 0;
+    static double max (double [] v){
+        double aux[] = Arrays.copyOf(v, v.length);
+        Arrays.sort(aux);
+        return aux[aux.length-1];
+    }
+    static double max_sort(double[] v){
+        double max = v[0];
+        for (int n = 1; n < v.length; n++){
+            if(v[n] > max)
+                max = v[n];
+        }
+        return max;
     }
     public static void main (String [ ] args){
         double [] v = new double [1000000];
         for (int i = 0; i < v.length; i++)
             v[i] = Math.random();
-        // long time = System.currentTimeMillis();
+        long time = System.currentTimeMillis();
+        double x = max(v);
+        System.out.println("Major = " + x + " Temps invertit: " + (System.currentTimeMillis() - time));
+        x = max_sort(v);
+        System.out.println("Major = " + x + " Temps invertit: " + (System.currentTimeMillis() - time));
+        }
     }
-}
