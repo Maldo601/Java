1b8a3bf2c887355b259c4e360b47dadbfd081bd8
Maldo601 <maldo601@gmail.com>
2021-04-20 21:52:11 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -935,6 +935,15 @@ static int[] fizzArray(int n) {
   }
   return v;
 }
+// Torna true si tots els numeros del Array son 1 o 4.
+static boolean only14(int[] nums) {
+  for ( int i = 0; i < nums.length; i ++){
+    if ( nums[i] != 1 && nums[i] != 4)
+      return false;
+  }
+  return true;
+}
+
 
 
 
