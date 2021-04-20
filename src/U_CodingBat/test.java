bb5bb00537eb3833c155d5ffdc8feaff568110af
Maldo601 <maldo601@gmail.com>
2021-04-20 23:01:36 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -991,6 +991,17 @@ static int matchUp(int[] nums1, int[] nums2) {
    }
    return counter;
 }
+static boolean has77(int[] nums) {
+  for (int i = 0; i < nums.length-1; i++){
+    if (nums[i] == 7 && nums[i+1] == 7)
+    return true;
+  }
+  for (int i = 0; i < nums.length-2; i++){
+    if (nums [i] == 7 && nums[i+2] == 7)
+    return true;
+  }
+  return false;
+}
 
   // MAIN 
   public static void main(String[] args) {
