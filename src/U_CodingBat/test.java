8933342c606d3cd720dd2e0efbe1b9cbd229faec
Maldo601 <maldo601@gmail.com>
2021-04-21 22:06:37 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -1075,6 +1075,29 @@ static String doubleChar(String str) {
   }
   return cad;
 }
+// Retorna un counter per cada vegada que apareix "hi"
+static int countHi(String str) {
+  int counter = 0;
+  for (int i = 0; i < str.length()-1; i++){
+    if ( str.substring(i, i+2).equals("hi"))
+      counter++;
+  }
+  return counter;
+}
+// Retorna true o false si "cat" apareix les mateixes vegades que "dog"
+static boolean catDog(String str) {
+  int dog = 0;
+  int cat = 0;
+  for ( int i = 0; i < str.length()-2; i++){
+    if (str.substring(i,i+3).equals("cat"))
+      cat++;
+    if (str.substring(i,i+3).equals("dog"))
+      dog++;
+  }
+  return (dog == cat) ? true : false;
+}
+
+
 
   // MAIN 
   public static void main(String[] args) {
