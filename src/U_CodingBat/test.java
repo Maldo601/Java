c8e265321b062f841571fe426dc37d64e19dd8ae
Maldo601 <maldo601@gmail.com>
2021-04-22 19:04:20 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -1242,6 +1242,36 @@ static String xyzMiddle(String str) {
   String pureMid = ""+str.charAt(str.length()/2-1) + str.charAt(str.length()/2) + str.charAt(str.length()/2+1);
   return pureMid;
 }
+// Torna el que estigue compres entre "bread"
+public String getSandwich(String str) {
+  int first = -1;
+  int last = -1;
+    
+  for(int i = 0; i < str.length() - 5; i++) {
+      if(str.substring(i, i + 5).equals("bread")) {
+          first = i;
+          break;
+      }
+  }                         
+  for(int i = str.length() - 5; i >= 0; i--) {
+      if(str.substring(i, i + 5).equals("bread")) {
+          last = i;
+          break;
+      }
+  }                                                    
+  if(first != -1 && last != -1 && first != last)
+      return str.substring(first + 5, last);                                                               
+  return "";
+}
+// Retorna true si el asterisco te les mateixes lletres davant o detras
+static boolean sameStarChar(String str) {
+
+  for (int i = 1; i < str.length()-1; i++){
+    if (str.charAt(i) == '*' && str.charAt(i-1) != str.charAt(i+1))
+      return false;
+  }
+  return true;
+}
 
   // MAIN 
   public static void main(String[] args) {
