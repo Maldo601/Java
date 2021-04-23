ac3f85c6cc52e2579e5d43f6009a4c5c10d193a4
Maldo601 <maldo601@gmail.com>
2021-04-23 03:32:22 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -1324,9 +1324,10 @@ public String zipZap(String str) {
 }
 
 
-
   // MAIN 
   public static void main(String[] args) {
- 
+    String a = "xxx";
+    String b = "X";
+    System.out.println(Arrays.toString(mixString1(a,b)));
   }
 }
