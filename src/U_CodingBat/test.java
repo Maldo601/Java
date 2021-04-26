9f0dc5cb2bcd322a564ea61c3e6d117a494c680d
Maldo601 <maldo601@gmail.com>
2021-04-27 00:45:11 +0200

    Masters

--- src/U_CodingBat/test.java
+++ src/U_CodingBat/test.java
@@ -1178,7 +1178,20 @@ static int[] pre4(int[] nums) {
   }
   return v;
 }
-
+// 
+public int[] post4(int[] nums) {
+  int i = nums.length - 1;
+  
+  while(nums[i] != 4)
+      i--;
+            
+  int[] arr = new int[nums.length - i - 1];
+                
+  for(int j = 0; j < arr.length; j++)
+      arr[j] = nums[i + j + 1];
+                        
+  return arr;
+}
 
 
 
