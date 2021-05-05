980451993c0b7282be77967f3133e7e4339c2c96
Maldo601 <maldo601@gmail.com>
2021-05-05 16:34:47 +0200

    Masters

--- src/X_LecturaFitxers/B_Primers100k.java
+++ src/X_LecturaFitxers/B_Primers100k.java
@@ -1,17 +1,34 @@
 import java.io.*;
 import java.util.Arrays;
-
 public class B_Primers100k {
     public static void main (String[] args){
-
-        long interval = System.currentTimeMillis();
+        // long interval = System.currentTimeMillis();
         BufferedReader bRead = null;
-        String fichero = "primes-to-100k.txt";
+        // BufferedReader zRead = null;
+        // String fichero = "primes-to-100k.txt";
         int linies = 0;
         String c;
         int counter = 0;
-        long v[] = new long[9592];
+            // Primer try, conta linies per determinar la longitud del vector
             try {
+                // Buffer
+                bRead = new BufferedReader
+                (
+                    new FileReader("C:/Users/maldo/Desktop/Java/src/X_LecturaFitxers/Ficheros/primes-to-100k.txt")
+                );
+                while((c = bRead.readLine()) != null)
+                    linies++;
+                /*
+                 * Es tanca el buffer perque el punter ha arribat al final i necessitarem
+                 * reoperar amb ell per introduir al vector els valors, tornant el punter al principi
+                 */ 
+                bRead.close();
+            }catch(Exception e){
+                System.out.println("Error.");
+            }
+            // Vector de Sortida.
+            long v[] = new long [linies];
+            try{
                 bRead = new BufferedReader
                 (
                     new FileReader("C:/Users/maldo/Desktop/Java/src/X_LecturaFitxers/Ficheros/primes-to-100k.txt")
@@ -20,10 +37,10 @@ public class B_Primers100k {
                     v[counter] = Integer.parseInt(c);
                     counter++;
                 }
-            }catch(Exception e){
+            }catch (Exception e){
                 System.out.println("blabla");
+            }finally{
+                System.out.println(Arrays.toString(v));
             }
-            System.out.println(Arrays.toString(v));
         }
     }
-
