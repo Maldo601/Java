5d4c8f5f8c4b2283a6860408c4b706d598bf911d
Maldo601 <maldo601@gmail.com>
2021-05-05 13:45:23 +0200

    Masters

--- src/X_LecturaFitxers/A_Primers100k.java
+++ src/X_LecturaFitxers/A_Primers100k.java
@@ -1,6 +1,36 @@
+import java.io.*;
+
 public class A_Primers100k {
-    
     public static void main (String[] args){
-        
+    // Comprovarem el temps d'execució del programa.
+    long interval = System.currentTimeMillis();
+    BufferedReader bRead = null;
+    int numLs = 0;
+    String fichero = "palabras.txt";
+    int c;
+        try {
+            /* Associarem el buffer al fitxer. Va aqui dintre perque de no existir
+               el fitxer, es un tros de codi susceptible a errors
+            */
+            bRead = new BufferedReader(new FileReader("palabras.txt"));
+            while((c = bRead.read()) != -1){
+                if ((char) c == '1')
+                    numLs++;
+            }
+        }catch ( Exception e ){
+            System.out.println("S'ha produit un error d'accés a " + fichero);
+            e.printStackTrace();
+        }finally {
+            System.out.println("Codi executat sempre: ");
+            // Al tancar fitxers tambe es poden produir errors.
+            try {
+                if (bRead != null)
+                    bRead.close();
+            }catch( Exception e ){
+                e.printStackTrace();
+            }
+        }
+        System.out.println("Time Exec: " + (System.currentTimeMillis() - interval)+" ms");
+        System.out.println("Trobades " + numLs + " lletres 'l' en el fitxer " + fichero);
     }
 }
