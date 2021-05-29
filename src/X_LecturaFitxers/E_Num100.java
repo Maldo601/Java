1be56b09d545c8e3cce50e5ca2004d51a58a0e1c
Maldo601 <maldo601@gmail.com>
2021-05-29 18:19:30 +0200

    Masters

--- src/X_LecturaFitxers/E_Num100.java
+++ src/X_LecturaFitxers/E_Num100.java
@@ -3,19 +3,24 @@ import java.io.*;
 public class E_Num100 {
     public static void main(String[] args){
         int counter = 0;
+        int countLines = 0;
+        int longitud = args[0].length()+1;
         File f = new File("Numeros1000.txt");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
                 for(int i = 1; i <= 100; i++){
-                    int x = (int)(Math.random()*1000) + 0;
+                    int x = (int)(Math.random()*Integer.parseInt(args[0])) + 0;
                     counter++;
-                    bw.write(String.format("%5d\t",x));
-                    if (i%10 == 0){
+                    bw.write(String.format("%"+String.valueOf(longitud)+"d\t",x));
+                    if (i%Integer.parseInt(args[2]) == 0){
                         bw.newLine();
+                        countLines++;
                     }
+                    if(countLines == Integer.parseInt(args[1]))
+                        break;
                 }
             }catch ( Exception ex ) {
                 System.out.println(ex.getMessage());
         }
-        System.out.println("\nImpresos " + counter + " resultats a " + f );
+        System.out.println("\nImpresos " + counter + " resultats a " + f + " en " + countLines + " files." );
     }
 }
