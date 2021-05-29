c0b1386e6a0b4b61a1051888e84fa6ca16d589b1
Maldo601 <maldo601@gmail.com>
2021-05-29 17:21:34 +0200

    Masters

--- src/X_LecturaFitxers/E_Num100.java
+++ src/X_LecturaFitxers/E_Num100.java
@@ -2,10 +2,12 @@ package X_LecturaFitxers;
 import java.io.*;
 public class E_Num100 {
     public static void main(String[] args){
+        int counter = 0;
         File f = new File("Numeros1000.txt");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
-                for(int i = 0; i < 100; i++){
+                for(int i = 1; i <= 100; i++){
                     int x = (int)(Math.random()*1000) + 0;
+                    counter++;
                     bw.write(String.format("%5d\t",x));
                     if (i%10 == 0){
                         bw.newLine();
@@ -14,5 +16,6 @@ public class E_Num100 {
             }catch ( Exception ex ) {
                 System.out.println(ex.getMessage());
         }
+        System.out.println("\nImpresos " + counter + " resultats a " + f );
     }
 }
