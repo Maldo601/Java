970011363c97031c2111124dabed6d1d81298860
Maldo601 <maldo601@gmail.com>
2021-05-29 17:15:10 +0200

    Masters

--- src/X_LecturaFitxers/E_Num100.java
+++ src/X_LecturaFitxers/E_Num100.java
@@ -4,9 +4,12 @@ public class E_Num100 {
     public static void main(String[] args){
         File f = new File("Numeros1000.txt");
         try (BufferedWriter bw = new BufferedWriter(new FileWriter(f))){
-                for(int i = 0; i < 1000; i++){
-                    int x = (int)(Math.random()*100) + 0;
-                    bw.write(String.format("%3d\n",x));
+                for(int i = 0; i < 100; i++){
+                    int x = (int)(Math.random()*1000) + 0;
+                    bw.write(String.format("%5d\t",x));
+                    if (i%10 == 0){
+                        bw.newLine();
+                    }
                 }
             }catch ( Exception ex ) {
                 System.out.println(ex.getMessage());
